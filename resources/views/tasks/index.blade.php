8ef364fe7a8972e95ead5042813b3e25c99f6225
Oleksii Halenko <axe.halenko@gmail.com>
2021-11-01 21:46:16 +0200

    view for table tasks

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -7,6 +7,44 @@
     <!-- Отображение ошибок проверки ввода -->
     @include('common.errors')
 </div>
+
 <a href="{{route('tasks.create')}}" class="btn btn-success"><i class="fa fa-plus"></i> Create new Task</a>
-<!-- TODO: Текущие задачи -->
+<br><br>
+
+<!-- Текущие задачи -->
+@if (count($tasks) > 0)
+<div class="panel panel-default">
+    <div class="panel-heading">
+        All tasks
+    </div>
+
+    <div class="panel-body">
+        <table class="table table-striped task-table">
+
+            <!-- Заголовок таблицы -->
+            <thead>
+                <th>Task Name</th>
+                <th>Remove</th>
+                <th>&nbsp;</th>
+            </thead>
+
+            <!-- Тело таблицы -->
+            <tbody>
+                @foreach ($tasks as $task)
+                <tr>
+                    <!-- Имя задачи -->
+                    <td class="table-text">
+                        <div>{{ $task->name }}</div>
+                    </td>
+
+                    <td>
+                        Delete {{ $task->id }}
+                    </td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+</div>
+@endif
 @endsection
